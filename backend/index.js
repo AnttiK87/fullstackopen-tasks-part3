4b361b5caae42a7329fee9392d58b5513c9642b9
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2024-10-09 14:27:38 +0300

    changes to part3

--- backend/index.js
+++ backend/index.js
@@ -28,11 +28,11 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-
+//enable Cross-Origin Resource Sharing and express library
 app.use(cors())
 app.use(express.json())
 
-// Conditionally apply logging to terminal custom morgan token is used for post reguest and morgan tiny for others
+// Conditionally apply logging to terminal custom morgan token is used for post and put reguest and morgan tiny for others
 app.use((req, res, next) => {
   if (req.method === 'POST' || req.method === 'PUT' ) {
     morgan(':method :url :status :res[content-length] - :response-time ms :body')(req, res, next)
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
   }
 })
 
+// Serve frontend from the 'dist' directory
 app.use(express.static('dist'))
 
 //Phonebook header
@@ -54,13 +55,13 @@ app.get('/info', (request, response, next) => {
   const dateString = currentDate.toString()
 
   Person.find({})
-      .then(persons => {
-          response.send(`
+    .then(persons => {
+      response.send(`
               <p>Phonebook has info for ${persons.length} people</p>
               <p>${dateString}</p>
           `)
-      })
-      .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 // Get all persons from the phonebook @ http://***/api/persons
@@ -68,7 +69,7 @@ app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // Get one person from the phonebook @ http://***/api/persons/:id
@@ -100,38 +101,60 @@ app.post('/api/persons', (request, response, next) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // Update person at the phonebook @ http://***/api/persons/:id
-app.put('/api/persons/:id', (request, response, next) => {
+app.put('/api/persons/:id', async (request, response, next) => {
   const body = request.body
 
-  const person = {
+  // Creating new Person model for validating
+  const validPerson = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+  try {
+    // Wait for validating to pass
+    await validPerson.validate()
+    // If validation is passed continue updating phone number
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+
+  } catch (error) {
+    //throw an error if validation is not ok
+    next(error)
+  }
 })
 
 // Delete person from the phonebook @ http://***/api/persons/:id
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
+      if (!result) {
+        // if person/id is not found from db return 404 for error handling
+        return response.status(404)
+      }
+      // else continue deleting
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
+//enable errorHandler middlevare and express librarys unknownEndpoint function
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// Set the server to listen on the specified PORT
+const PORT = process.env.PORT // Get the PORT from environment variables
+app.listen(PORT, () => { // Start the server and listen for incoming requests
+  console.log(`Server running on port ${PORT}`) // Log the server's running status
 })
--- backend/models/person.js
+++ backend/models/person.js
@@ -1,24 +1,47 @@
 const mongoose = require('mongoose')
 
+// set mongoose to query syntax
 mongoose.set('strictQuery', false)
 
+// get MongoDB connection from .env
 const url = process.env.MONGODB_URI
 
+// show status on console
 console.log('connecting to', url)
+
+// connect to MongoDB database and show status on console
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
+// define model and validation for items in db
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      //custom validator for phone number
+      validator: function(v) {
+        const phoneNroPattern = /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})$/
 
-  personSchema.set('toJSON', {
+        return phoneNroPattern.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  },
+})
+
+// customize the JSON representation, don't show id and version
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -26,4 +49,5 @@ mongoose.connect(url)
   }
 })
 
+// export the Person model
 module.exports = mongoose.model('Person', personSchema)
