47708f3a540fff7b8cb56e69d384a8c2378b8d5f
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2024-10-09 15:09:29 +0300

    changes to part3

--- backend/index.js
+++ backend/index.js
@@ -52,13 +52,29 @@ app.get('/', (request, response) => {
 //Phonebook info @ http://***/info
 app.get('/info', (request, response, next) => {
   const currentDate = new Date()
-  const dateString = currentDate.toString()
 
+  // Formatting date to 'wd mon dd YYYY hh:mm:ss GMT+0200 (Eastern European Standard Time)'
+  const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+  const dayName = weekDays[currentDate.getUTCDay()]
+  const monthName = months[currentDate.getUTCMonth()]
+  const day = currentDate.getUTCDate()
+  const year = currentDate.getUTCFullYear()
+
+  const timePart = currentDate.toLocaleTimeString('en-GB', { hour12: false })
+
+  const timeZoneOffset = '+0200'
+  const timeZoneName = '(Eastern European Standard Time)'
+
+  const formattedDate = `${dayName} ${monthName} ${day} ${year} ${timePart} GMT${timeZoneOffset} ${timeZoneName}`
+
+  //Show people count in phonebook db and formatted curret date and time
   Person.find({})
     .then(persons => {
       response.send(`
               <p>Phonebook has info for ${persons.length} people</p>
-              <p>${dateString}</p>
+              <p>${formattedDate}</p>
           `)
     })
     .catch(error => next(error))
